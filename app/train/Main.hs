77f8c915514f53a1929a3a4e341a74985da8d7a5
miyagawananako <miyagawa775@gmail.com>
2024-11-21 18:29:30 +0900

    create IntermediateConstructor

--- app/train/Main.hs
+++ app/train/Main.hs
@@ -17,49 +17,61 @@ loadActionsFromBinary filepath = do
 hash :: T.Text -> Int
 hash = T.foldl' (\acc c -> acc * 31 + fromEnum c) 0
 
-embedPreterm :: U.Preterm -> [Int]
-embedPreterm preterm = case preterm of
-  U.Var j  -> [1, j]
-  U.Con c  -> [2, hash c]
-  U.Type   -> [3]
-  U.Kind   -> [4]
-  U.Pi a b -> [5] ++ embedPreterm a ++ embedPreterm b
-  U.Lam m      -> [6] ++ embedPreterm m
-  U.App m n    -> [7] ++ embedPreterm m ++ embedPreterm n
-  U.Not m  -> [8] ++ embedPreterm m
-  U.Sigma a b  -> [9] ++ embedPreterm a ++ embedPreterm b
-  U.Pair m n   -> [10] ++ embedPreterm m ++ embedPreterm n
+data IntermediateConstructor = EOSig | EOCon | EOTerm | EOTyp | FST | SND | LPAREN | RPAREN | OTHER | Var'0 | Var'1 | Var'2 | Con' | Type' | Kind' | Pi' | Lam' | App' | Not' | Sigma' | Pair' | Proj' | Disj' | Iota' | Unpack' | Bot' | Unit' | Top' | Entity' | Nat' | Zero' | Succ' | Natrec' | Eq' | Refl' | Idpeel'
+  deriving (Enum, Show)
+
+splitPreterm :: U.Preterm -> [IntermediateConstructor]
+splitPreterm preterm = case preterm of
+  U.Var j  ->
+    case j of
+      0 -> [LPAREN] ++ [Var'0] ++ [RPAREN]
+      1 -> [LPAREN] ++ [Var'1] ++ [RPAREN]
+      2 -> [LPAREN] ++ [Var'2] ++ [RPAREN]
+      _ -> [LPAREN] ++ [OTHER] ++ [RPAREN]
+  U.Con c  -> [LPAREN] ++ [Con'] ++ [RPAREN]
+  U.Type   -> [LPAREN] ++ [Type'] ++ [RPAREN]
+  U.Kind   -> [LPAREN] ++ [Kind'] ++ [RPAREN]
+  U.Pi a b -> [LPAREN] ++ [Pi'] ++ splitPreterm a ++ splitPreterm b ++ [RPAREN]
+  U.Lam m      -> [LPAREN] ++ [Lam'] ++ splitPreterm m ++ [RPAREN]
+  U.App m n    -> [LPAREN] ++ [App'] ++ splitPreterm m ++ splitPreterm n ++ [RPAREN]
+  U.Not m  -> [LPAREN] ++ [Not'] ++ splitPreterm m ++ [RPAREN]
+  U.Sigma a b  -> [LPAREN] ++ [Sigma'] ++ splitPreterm a ++ splitPreterm b ++ [RPAREN]
+  U.Pair m n   -> [LPAREN] ++ [Pair'] ++ splitPreterm m ++ splitPreterm n ++ [RPAREN]
   U.Proj s m   ->
     case s of
-      U.Fst -> [11, 1] ++ embedPreterm m
-      U.Snd -> [11, 2] ++ embedPreterm m
-  U.Disj a b   -> [12] ++ embedPreterm a ++ embedPreterm b
+      U.Fst -> [LPAREN] ++ [Proj'] ++ [FST] ++ splitPreterm m ++ [RPAREN]
+      U.Snd -> [LPAREN] ++ [Proj'] ++ [SND] ++ splitPreterm m ++ [RPAREN]
+  U.Disj a b   -> [LPAREN] ++ [Disj'] ++ splitPreterm a ++ splitPreterm b ++ [RPAREN]
   U.Iota s m   ->
     case s of
-      U.Fst -> [13, 1] ++ embedPreterm m
-      U.Snd -> [13, 2] ++ embedPreterm m
-  U.Unpack p h m n -> [14] ++ embedPreterm p ++ embedPreterm h ++ embedPreterm m ++ embedPreterm n
-  U.Bot        -> [15]
-  U.Unit       -> [16]
-  U.Top        -> [17]
-  U.Entity     -> [18]
-  U.Nat        -> [19]
-  U.Zero       -> [20]
-  U.Succ n     -> [21] ++ embedPreterm n
-  U.Natrec n e f -> [22] ++ embedPreterm n ++ embedPreterm e ++ embedPreterm f
-  U.Eq a m n   -> [23] ++ embedPreterm a ++ embedPreterm m ++ embedPreterm n
-  U.Refl a m   -> [24] ++ embedPreterm a ++ embedPreterm m
-  U.Idpeel m n -> [25] ++ embedPreterm m ++ embedPreterm n
+      U.Fst -> [LPAREN] ++ [Iota'] ++ [FST] ++ splitPreterm m ++ [RPAREN]
+      U.Snd -> [LPAREN] ++ [Iota'] ++ [SND] ++ splitPreterm m ++ [RPAREN]
+  U.Unpack p h m n -> [LPAREN] ++ [Unpack'] ++ splitPreterm p ++ splitPreterm h ++ splitPreterm m ++ splitPreterm n ++ [RPAREN]
+  U.Bot        -> [LPAREN] ++ [Bot'] ++ [RPAREN]
+  U.Unit       -> [LPAREN] ++ [Unit'] ++ [RPAREN]
+  U.Top        -> [LPAREN] ++ [Top'] ++ [RPAREN]
+  U.Entity     -> [LPAREN] ++ [Entity'] ++ [RPAREN]
+  U.Nat        -> [LPAREN] ++ [Nat'] ++ [RPAREN]
+  U.Zero       -> [LPAREN] ++ [Zero'] ++ [RPAREN]
+  U.Succ n     -> [LPAREN] ++ [Succ'] ++ splitPreterm n ++ [RPAREN]
+  U.Natrec n e f -> [LPAREN] ++ [Natrec'] ++ splitPreterm n ++ splitPreterm e ++ splitPreterm f ++ [RPAREN]
+  U.Eq a m n   -> [LPAREN] ++ [Eq'] ++ splitPreterm a ++ splitPreterm m ++ splitPreterm n ++ [RPAREN]
+  U.Refl a m   -> [LPAREN] ++ [Refl'] ++ splitPreterm a ++ splitPreterm m ++ [RPAREN]
+  U.Idpeel m n -> [LPAREN] ++ [Idpeel'] ++ splitPreterm m ++ splitPreterm n ++ [RPAREN]
+
+
+embedPreterm :: U.Preterm -> [Int]
+embedPreterm preterm = map fromEnum $ splitPreterm preterm
 
 embedPreterms :: [U.Preterm] -> [Int]
-embedPreterms preterms = concatMap (\preterm -> embedPreterm preterm ++ [-1]) preterms
+embedPreterms preterms = concatMap (\preterm -> embedPreterm preterm) preterms
 
 embedSignature :: U.Signature -> [Int]
-embedSignature signature = concatMap (\(name, preterm) -> [hash name] ++ embedPreterm preterm ++ [-1]) signature
+embedSignature signature = concatMap (\(name, preterm) -> [fromEnum LPAREN] ++ [hash name] ++ embedPreterm preterm ++ [fromEnum RPAREN]) signature
 
 main :: IO()
 main = do
   trainingData <- loadActionsFromBinary saveFilePath
 
-  let judgmentData = map (\(judgment, _) -> (embedSignature $ U.signtr judgment) ++ [-2] ++ (embedPreterms $ U.contxt judgment) ++ [-3] ++ (embedPreterm $ U.trm judgment) ++ [-4] ++ (embedPreterm $ U.typ judgment)) trainingData
+  let judgmentData = map (\(judgment, _) -> (embedSignature $ U.signtr judgment) ++ [fromEnum EOSig] ++ (embedPreterms $ U.contxt judgment) ++ [fromEnum EOCon] ++ (embedPreterm $ U.trm judgment) ++ [fromEnum EOTerm] ++ (embedPreterm $ U.typ judgment) ++ [fromEnum EOTyp]) trainingData
   print judgmentData
